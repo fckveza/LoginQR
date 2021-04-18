437476d4238ada9a22c8c87edfadb500868cc962
VE ZA <36743311+fckveza@users.noreply.github.com>
2021-04-18 20:32:00 +0700

    Update loginQrLine.py

--- loginQrLine.py
+++ loginQrLine.py
@@ -11,45 +11,39 @@ headerLine = {
 }
 
 def NewLogin():
-  sagne = requests.post(hostVH+"loginqr="+apikey, json=headerLine)
+  sagne = requests.post(hostVH+"zloginqr="+apikey, json=headerLine)
   if sagne.status_code == 200:
      tH = sagne.json()
      print("Link Login: "+tH["result"]["url"]) #Atau ganti dengan client.sendMessage(to,tH["result"]["url"])
-  else:
-     print("Error : ", sagne.status_code)
-  while "sagne" not in sagne:
-    time.sleep(4)
-    ve = requests.get(hostVH+"getcode="+apikey)
-    if ve.status_code == 200:
-      bG = ve.json()
-      if bG["result"]["code"] != "":
+  time.sleep(4)
+  ve = requests.get(hostVH+"zgetcode="+apikey)
+  if ve.status_code == 200:
+    bG = ve.json()
+    if bG["result"]["code"] != "":
         print("Pin code : ",bG["result"]["code"])
-        break
-  while "sagne" not in ve:
-    time.sleep(4)
-    ve = requests.get(hostVH+"gettoken="+apikey)
-    if ve.status_code == 200:
+  time.sleep(4)
+  ve = requests.get(hostVH+"zgettoken="+apikey)
+  if ve.status_code == 200:
       rH = ve.json()
       if rH["result"]["token"] != "":
          print("Token : ",rH["result"]["token"]) #Atau ganti dengan client.sendMessage(to,rH["result"]["token"])
          print("Cert : ",rH["result"]["certificate"]) #Atau ganti dengan client.sendMessage(to,rH["result"]["certificate"])
-         break
+         
   
 def LoginWithCertificate(cert):
-  sagne = requests.post(hostVH+"loginqrwithcert="+apikey+"&"+cert, json=headerLine)
+  sagne = requests.post(hostVH+"zloginqrwithcert="+apikey+"&"+cert, json=headerLine)
   if sagne.status_code == 200:
      ve = sagne.json()
      print("Link Login: "+ve["result"]["url"]) #Atau ganti dengan client.sendMessage(to,ve["result"]["url"])
   else:
      print("Error : ", sagne.status_code)
-  while "sagne" not in sagne:
-    time.sleep(4)
-    ve = requests.get(hostVH+"gettoken="+apikey)
-    if ve.status_code == 200:
+  time.sleep(4)
+  ve = requests.get(hostVH+"gettoken="+apikey)
+  if ve.status_code == 200:
       cok = ve.json()
       if cok["result"]["token"] != "":
         print("Token : ",cok["result"]["token"]) #Atau ganti dengan client.sendMessage(to,cok["result"]["token"])
-        break
+        
   
 #LoginWithCertificate("certificate mu")
 #NewLogin()
